39001456cfa7031dabf4e6be8689cc98b7876783
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-20 15:39:54 +0200

    The initial commit.

---
+++ src/main/java/bm/app/springplayfield/SpringPlayfieldApplication.java
@@ -0,0 +1,13 @@
+package bm.app.springplayfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayfieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlayfieldApplication.class, args);
+    }
+
+}
