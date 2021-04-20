d335b2583c3c9e18c2e5a47968d0129d84cbe01c
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-20 19:57:07 +0200

    Tried ApplicationContext's creation of a Spring Container and thus automated creation of an object without manually doing so.

--- src/main/java/bm/app/springplayfield/SpringPlayfieldApplication.java
+++ src/main/java/bm/app/springplayfield/SpringPlayfieldApplication.java
@@ -7,22 +7,35 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 @SpringBootApplication
 public class SpringPlayfieldApplication {
 
     public static void main(String[] args) {
+        /**
+         * Automatically creates a Spring Container for me (ApplicationContext)?
+         */
         ApplicationContext context = SpringApplication.run(SpringPlayfieldApplication.class, args);
 
         Dwarf dwarf = context.getBean(Dwarf.class);
         dwarf.attackTheEnemy();
 
-        // Instead of ApplicationContext
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+       /**
+        * Instead of ApplicationContext - an archaic solution with BeanFactory (requires spring.xml file within the
+        * root directory, not the resources one.
+        *
+        *         BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        *
+        *         Elf elf = (Elf) factory.getBean("elf");
+        *         elf.attackTheFoe();
+        */
 
-        Elf elf = (Elf) factory.getBean("elf");
+        ApplicationContext contextNew = new ClassPathXmlApplicationContext("spring.xml");
+        Elf elf = (Elf) contextNew.getBean("elf");
         elf.attackTheFoe();
+
 
     }
 }
---
+++ src/main/java/bm/app/springplayfield/models/Orc.java
@@ -0,0 +1,11 @@
+package bm.app.springplayfield.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Orc {
+
+    public Orc() {
+        System.out.println("I, an orc, was summoned by dark warlocks of Spring without ever being called manually by the user!");
+    }
+}
